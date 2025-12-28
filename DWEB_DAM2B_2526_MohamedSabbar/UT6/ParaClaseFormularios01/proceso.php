ff4d5405c67b548c59b8f7071a27f51ac8c4a67b
msabbar1 <msabbar1@educacion.navarra.es>
2025-12-28 20:51:46 +0100

    tarea 1 en formularios

---
+++ DWEB_DAM2B_2526_MohamedSabbar/UT6/ParaClaseFormularios01/proceso.php
@@ -0,0 +1,55 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="estilo.css" />
+    <title>Formulario datos personales</title>
+</head>
+
+<body>
+    <div id="contenido">
+        <?php
+        echo "<pre>";
+        print_r($_POST);
+        echo "</pre>";
+
+        $usuario = isset($_POST['usuario']) ? $_POST['usuario'] : '';
+        $clave = isset($_POST['clave']) ? $_POST['clave'] : '';
+        $sexo = isset($_POST['sexo']) ? $_POST['sexo'] : '';
+        $edad = isset($_POST['edad']) ? $_POST['edad'] : '';
+
+        $vivienda = isset($_POST['vivienda']) ? $_POST['vivienda'] : 'No marcado';
+        $trabajo = isset($_POST['trabaja']) ? $_POST['trabaja'] : 'No marcado';
+        $mus = isset($_POST['mus']) ? $_POST['mus'] : 'No marcado';
+
+        $comentarios = isset($_POST['comentarios']) ? $_POST['comentarios'] : '';
+        $pais = isset($_POST['pais']) ? $_POST['pais'] : '';
+
+        
+        $colores = isset($_POST['colores']) ? $_POST['colores'] : [];
+
+        echo "El usuario ha introducido esta info :<br>";
+        echo "Nombre : $usuario<br>";
+        echo "Password : $clave <br>";
+        echo "Sexo : $sexo<br>";
+        echo "Edad : $edad<br>";
+        echo "Vivienda : $vivienda<br>";
+        echo "Trabajo : $trabajo<br>";
+        echo "Juegos : $mus<br>";
+        echo "Comentarios : $comentarios<br>";
+        echo "Pais : $pais<br>";
+        echo "Sus colores favoritos: ";
+        if (!empty($colores)) {
+            foreach ($colores as $color) {
+                echo $color . " ";
+            }
+        } else {
+            echo "No ha seleccionado ninguno";
+        }
+        ?>
+
+    </div>
+</body>
+
+</html>
